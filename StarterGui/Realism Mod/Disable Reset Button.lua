3573ae80a0b59f236af6d11cc10531724ee62733
Dynamo <chickabamboombam@gmail.com>
2025-05-22 02:10:59 -0400

    Updated StarterGui/Realism Mod/Disable Reset Button.lua

---
+++ StarterGui/Realism Mod/Disable Reset Button.lua
@@ -0,0 +1,10 @@
+-- @ScriptType: LocalScript
+--[[
+	Created by foodman54
+]]
+
+-- Services
+local starterGui = game:GetService("StarterGui")
+
+task.wait(1)
+starterGui:SetCore("ResetButtonCallback", false)
